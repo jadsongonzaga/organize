7726096a7dbd7222581a7da2945287dae5994860
jadsongonzaga <jadson@jadson-deb>
2017-05-08 06:56:02 -0300

    Inserts necessário para o funcionamento do sistema

---
+++ script/insert_diversos.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  jadson
+ * Created: 07/05/2017
+ */
+
+INSERT INTO usuario (tipo, nome, login, senha) VALUES (1, 'Administrador', 'adm', 'adm');
